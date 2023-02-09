1ae7f3ddeea4656171a12e7ab1bd818d4eaab3c5
NNiboshi <iwanaga2.1.2@gmail.com>
2023-02-09 18:48:12 +0900

    test

---
+++ src/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    reactStrictMode: true,
+    swcMinify: true,
+  },
+}
+
+
+module.exports = {
+  images: {
+  unoptimized: true,
+  },
+  basePath: '/[GitHubのリポジトリ名]',
+}
