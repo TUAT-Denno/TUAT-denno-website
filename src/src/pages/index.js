5e09c3be9781b63c9dd9226df8b41ad71634dfd6
NNiboshi <iwanaga2.1.2@gmail.com>
2023-02-09 22:11:14 +0900

    ver1.0.0

---
+++ src/src/pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Index = () => {
+  return (
+    <>
+      <title>農工大　電脳サークルへようこそ!</title>
+      <head>
+        <h1>＼農工大電脳サークルのサイトです／</h1>
+      </head>
+      <body>
+        <p1>TUAT-Denno official website ver1.0.0</p1>
+      </body>
+    </>
+  )
+}
+
+export default Index
