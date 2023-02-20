1955e718f66169049fdbf4bf48674d0a5c71d56f
NNiboshi <iwanaga2.1.2@gmail.com>
2023-02-20 11:00:49 +0900

    ver0.2.1

--- src/src/pages/gallery.js
+++ src/src/pages/gallery.js
@@ -11,27 +11,27 @@ const About = () => {
         <ul>
           <li>
             <Link href="/">
-              <p>電脳サークル</p>
+              <p><font size="2" face="Impact">電脳サークル</font></p>
             </Link>
           </li>
           <li>
             <Link href="/about">
-              <p>About</p>
+              <p><font size="2">About</font></p>
             </Link>
           </li>
           <li>
             <Link href="/gallery">
-              <p>Gallery</p>
+              <p><font size="2">Gallery</font></p>
             </Link>
           </li>
           <li>
             <Link href="/faq">
-              <p>FAQ</p>
+              <p><font size="2">FAQ</font></p>
             </Link>
           </li>
           <li>
             <Link href="/join">
-              <p>Join</p>
+              <p><font size="2">Join</font></p>
             </Link>
           </li>
         </ul>
