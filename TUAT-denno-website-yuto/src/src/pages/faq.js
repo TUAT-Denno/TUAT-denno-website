770fdb6a003de4e0b5d285546d6d889bd38d68f6
Yuto-A <s228450x@st.go.tuat.ac.jp>
2023-02-21 22:46:14 +0900

    tanomu

---
+++ TUAT-denno-website-yuto/src/src/pages/faq.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+
+const About = () => {
+    return(
+      <>
+      <header>
+        <title>東京農工大学電脳サークル</title>
+      </header>
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <p><font size="2" face="Impact">電脳サークル</font></p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <p><font size="2">About</font></p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/gallery">
+              <p><font size="2">Gallery</font></p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/faq">
+              <p><font size="2">FAQ</font></p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/join">
+              <p><font size="2">Join</font></p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <footer>
+        <h1>FAQ Page</h1>
+      </footer>
+      </>
+    )
+}
+export default About
