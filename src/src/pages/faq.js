cb663dbf3dbef0e1943e7685854a501546ba8dd6
Yuto-A <s228450x@st.go.tuat.ac.jp>
2023-02-23 00:18:03 +0900

    ver1.3.7

--- src/src/pages/faq.js
+++ src/src/pages/faq.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Link from "next/link"
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+// import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
 
 import HEADER from './components/header'
 import FOOTER from './components/footer'
