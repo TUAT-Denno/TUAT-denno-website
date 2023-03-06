253a6f8d1dc3eccc8739c6908b22e74504b545e7
fujino <toufu24program@gmail.com>
2023-03-07 01:02:58 +0900

    add npminstall

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <title>東京農工大学 電脳サークル</title>
         <meta name="description" content="東京農工大学電脳サークルの紹介ページ" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/dennofavicon.png" />
       </Head>
 
       <HEADER />
