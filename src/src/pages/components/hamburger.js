34eb9830486e300b4e18e352933b81ae88ed5326
fujino <toufu24program@gmail.com>
2023-03-22 16:52:28 +0900

    ページ開時のアニメーション削除

--- src/src/pages/components/hamburger.js
+++ src/src/pages/components/hamburger.js
@@ -6,13 +6,17 @@ import Image from 'next/image'
 import denno_icon from 'public/denno-ico.png'
 import twitter_icon from 'public/Twitter_icon.png'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function HAMBURGER() {
     const [isOpen, setIsOpen] = useState(false)
+    const [isAnimated, setIsAnimated] = useState(false)
 
+    // ハンバーガーメニューが開いているかを判定
     const toggleOpen = () => {
         setIsOpen(!isOpen)
+        //ページを開いたときにアニメーションが働かないように，ボタンが押されたタイミングで処理を開始
+        setIsAnimated(true);
     }
 
     return (
@@ -20,8 +24,8 @@ export default function HAMBURGER() {
             <div className={styles.menu_icon_container}>
                 {/* ハンバーガーマーク */}
                 <div className={styles.hamburger_icon} onClick={toggleOpen}>
-                    <div className={isOpen ? styles.hamburger_icon_disappear : ''}>
-                        <div className={!isOpen ? styles.hamburger_icon_appear : ''}>
+                    <div className={!isOpen && isAnimated ? styles.hamburger_icon_appear : ''}>
+                        <div className={isOpen && isAnimated ? styles.hamburger_icon_disappear : ''}>
                             <div className={styles.hamburger_icon_line} />
                             <div className={styles.hamburger_icon_line} />
                             <div className={styles.hamburger_icon_line} />
@@ -30,58 +34,62 @@ export default function HAMBURGER() {
                 </div>
                 {/* ばつマーク */}
                 <div className={styles.cross_icon} onClick={toggleOpen}>
-                    <div className={!isOpen ? styles.cross_icon_disappear : ''}>
-                        <div className={isOpen ? styles.cross_icon_appear : ''}>
-                            <div className={styles.cross_icon_line} style={{
-                                transform: 'rotate(45deg)',
-                                position: 'absolute'
-                            }} />
-                            <div className={styles.cross_icon_line} style={{ transform: 'rotate(-45deg)' }} />
+                    <div className={!isOpen && isAnimated ? styles.cross_icon_disappear : ''}>
+                        <div style={isAnimated ? {} : { opacity: '0' }}>
+                            <div className={isOpen ? styles.cross_icon_appear : ''}>
+                                <div className={styles.cross_icon_line} style={{
+                                    transform: 'rotate(45deg)',
+                                    position: 'absolute'
+                                }} />
+                                <div className={styles.cross_icon_line} style={{ transform: 'rotate(-45deg)' }} />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
-            {isOpen && (
-                //メニュー
-                <div className={styles.menu}>
-                    <Link href="/">
-                        <li>HOME</li>
-                    </Link>
+            {
+                isOpen && (
+                    //メニュー
+                    <div className={styles.menu}>
+                        <Link href="/">
+                            <li>HOME</li>
+                        </Link>
 
-                    <Link href="/about">
-                        <li>ABOUT</li>
-                    </Link>
+                        <Link href="/about">
+                            <li>ABOUT</li>
+                        </Link>
 
-                    <Link href="/gallery">
-                        <li>GALLERY</li>
-                    </Link>
+                        <Link href="/gallery">
+                            <li>GALLERY</li>
+                        </Link>
 
-                    <Link href="/faq">
-                        <li>FAQ</li>
-                    </Link>
+                        <Link href="/faq">
+                            <li>FAQ</li>
+                        </Link>
 
-                    <Link href="/access">
-                        <li>ACCESS</li>
-                    </Link>
+                        <Link href="/access">
+                            <li>ACCESS</li>
+                        </Link>
 
-                    {/* リンクを入部フォームにする */}
-                    <a href="https://www.tuat.ac.jp/" target="_blank" rel="noopener noreferrer">
-                        <li>JOIN</li>
-                    </a>
-                    <a href="https://twitter.com/noko_denno" target="_blank" rel="noopener noreferrer">
-                        <li><i className="fa-brands fa-twitter"></i>
-                            <Image
-                                priority={true}
-                                src={twitter_icon}
-                                alt="icon"
-                                className={styles.twlogo}
-                            /></li>
-                    </a>
+                        {/* リンクを入部フォームにする */}
+                        <a href="https://www.tuat.ac.jp/" target="_blank" rel="noopener noreferrer">
+                            <li>JOIN</li>
+                        </a>
+                        <a href="https://twitter.com/noko_denno" target="_blank" rel="noopener noreferrer">
+                            <li><i className="fa-brands fa-twitter"></i>
+                                <Image
+                                    priority={true}
+                                    src={twitter_icon}
+                                    alt="icon"
+                                    className={styles.twlogo}
+                                /></li>
+                        </a>
 
-                    <button onClick={toggleOpen}>Close</button>
-                </div>
-            )}
-        </div>
+                        <button onClick={toggleOpen}>Close</button>
+                    </div>
+                )
+            }
+        </div >
     )
 }
