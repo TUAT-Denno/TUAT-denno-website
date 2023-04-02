dbadfd16d8bba5a66a7ab3f83c6f8576034f068f
toufu-24 <toufu24program@gmail.com>
2023-04-03 07:50:33 +0900

    Home配置変更

--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -41,30 +41,30 @@ export default function Home() {
         <link rel="icon" href="public/dennofavicon.png" />
       </Head>
       <main>
-        <div className={styles.main_container}>
+        <div className={styles.hometitle_container}>
           <div className={styles.hometitle}>
-            <h1>東京農工大学 電脳サークルへようこそ！！</h1>
-          </div>
-            {/*とりあえず適当なコードですが，もしあったらRealBoardとかのコードとかの方が良いかも*/}
-          <div className={styles.picture_container}>
-            <Image
-              src={windowWidth > 750 ? code_pic : code_numberless_pic}
-              alt="code"
-              className={styles.picture1}
-            />
-
-            <Image
-              src={RealBoard_pic}
-              alt="Real Board"
-              className={styles.picture2}
-            />
+            <h1>東京農工大学 電脳サークルへようこそ！</h1>
           </div>
         </div>
-        
+        {/*とりあえず適当なコードですが，もしあったらRealBoardとかのコードとかの方が良いかも*/}
+        <div className={styles.picture_container}>
+          <Image
+            src={windowWidth > 750 ? code_pic : code_numberless_pic}
+            alt="code"
+            className={styles.picture1}
+          />
+
+          <Image
+            src={RealBoard_pic}
+            alt="Real Board"
+            className={styles.picture2}
+          />
+        </div>
+
         <HEADER />
         <div className={styles.About_container}>
           <Image
-            src={windowWidth > 750 ? code_pic : code_numberless_pic}
+            src={code_pic}
             alt="code"
             className={styles.About_picture}
           />
